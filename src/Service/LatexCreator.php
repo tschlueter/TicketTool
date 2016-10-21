292628fb6abb4a5cd59c8768da91b11cc391fd91
Christopher Stock <email@christopherstock.de>
2016-10-21 15:11:15 +0200

    Added latex file creation and conversion to pdf.

--- src/Service/LatexCreator.php
+++ src/Service/LatexCreator.php
@@ -10,15 +10,22 @@ class Service_LatexCreator
      */
     public static function test()
     {
-        $fileName = 'out/latextTest.tx';
+        // create latex file
+
+        $fileNameTex = 'out/latexTest.tx';
         $data = (
-            '\documentclass{article}'   . "\n"
-            . '\begin{document}'            . "\n"
-            . '    Hello World!'                . "\n"
-            . '\end{document}'              . "\n"
+            '\documentclass{article}' . "\n"
+            . '\begin{document}'      . "\n"
+            . '    Hello World!'      . "\n"
+            . '\end{document}'        . "\n"
         );
 
-        file_put_contents($fileName, $data);
+        file_put_contents($fileNameTex, $data);
+
+        // render to pdf
+
+        $command = 'pdflatex -output-directory=out ' . $fileNameTex . ' ';
+        $output = shell_exec($command);
     }
 
 }
