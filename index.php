607a8645357f01830df46eccf6339af9f8b65fbd
Christopher Stock <email@christopherstock.de>
2016-10-21 13:11:07 +0200

    Implemented QR code lib and completed its generation.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once( 'src/Autoloader.php' );
+require_once( 'lib/phpqrcode.php' );
 
 Autoloader::init();
 Controller_TicketTool::main();
--- src/Controller/TicketTool.php
+++ src/Controller/TicketTool.php
@@ -16,27 +16,40 @@ class Controller_TicketTool
     {
         echo 'BAHAG JIRA TicketTool v.' . self::VERSION . '<br><br>';
 
-        $ticket = (array_key_exists('ticket', $_GET) ? $_GET['ticket'] : null);
-        $user   = (array_key_exists('user',   $_GET) ? $_GET['user']   : null);
-        $pass   = (array_key_exists('pass',   $_GET) ? $_GET['pass']   : null);
+        // browse parameters TODO extract to Parameters model class
+
+        $ticketId = (array_key_exists('ticket', $_GET) ? $_GET['ticket'] : null);
+        $user     = (array_key_exists('user',   $_GET) ? $_GET['user']   : null);
+        $pass     = (array_key_exists('pass',   $_GET) ? $_GET['pass']   : null);
 
         if (
-                $ticket == null
-            ||  $user   == null
-            ||  $pass   == null
+                $ticketId == null
+            ||  $user     == null
+            ||  $pass     == null
         ) {
             die('Please specify GET-Parameters [ticket][user][pass] for the tool to operate.');
         }
 
+        // pick ticket
+
         $ticket = Service_JiraTicketImporter::get(
-            $ticket,
+            $ticketId,
             $user,
             $pass
         );
-
         echo 'Picked ticket [' . $ticket->getId() . '] with title [' . $ticket->getTitle() . ']<br><br>';
 
-        //$qrImage = Service_
+        // create qr code as png image
+        $ticketUrl = self::JIRA_BASE_URL . '/browse/' . $ticket->getId();
+        $fileName  = 'filename.png';
+        QRcode::png(
+            $ticketUrl,
+            $fileName
+        );
+
+        echo 'Successfully created QR code.<br><br>';
+        echo '<img src="' . $fileName . '">';
+
 
 
     }
