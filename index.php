c1abdea743bce2384c24519dc572e954f00ef847
Christopher Stock <email@christopherstock.de>
2016-10-21 09:37:15 +0200

    Initial commitment.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+    require_once( 'src/TicketTool.php' );
+
+    // Invoke the TicketTool's main method.
+    TicketTool::main();
---
+++ src/TicketTool.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+     * A tool for printing tickets.
+     */
+    class TicketTool
+    {
+
+        /**
+         * The application's entry point.
+         */
+        public static function main()
+        {
+            echo 'Welcome!';
+
+
+
+
+        }
+
+    }
