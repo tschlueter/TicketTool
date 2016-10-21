c1221974ec6621bd7dc151f9fd409556d9ab2717
Christopher Stock <email@christopherstock.de>
2016-10-21 15:22:01 +0200

    Trying new LaTeX template.

--- src/Service/LatexCreator.php
+++ src/Service/LatexCreator.php
@@ -10,22 +10,24 @@ class Service_LatexCreator
      */
     public static function test()
     {
+        // include latex template
+
+        $fileNameTexIn  = 'in/latexTemplate.tx';
+        $fileNameTexOut = 'out/latexTest.tx';
+
+        $data = file_get_contents($fileNameTexIn);
+
+
         // create latex file
 
-        $fileNameTex = 'out/latexTest.tx';
-        $data = (
-            '\documentclass{article}' . "\n"
-            . '\begin{document}'      . "\n"
-            . '    Hello World!'      . "\n"
-            . '\end{document}'        . "\n"
-        );
-
-        file_put_contents($fileNameTex, $data);
+        file_put_contents($fileNameTexOut, $data);
 
         // render to pdf
 
-        $command = 'pdflatex -output-directory=out ' . $fileNameTex . ' ';
+        $command = 'pdflatex -output-directory=out ' . $fileNameTexOut . ' ';
         $output = shell_exec($command);
+
+        echo 'Latex generation output is [' . $output . ']<br><br><hr><br>';
     }
 
 }
