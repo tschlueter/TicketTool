dac43f6032ff6bed70fce6ae72c712e222042bb5
Christopher Stock <email@christopherstock.de>
2016-10-21 19:25:23 +0200

    Revising output PDF.

--- src/Service/PdfCreator.php
+++ src/Service/PdfCreator.php
@@ -20,14 +20,22 @@ class Service_PdfCreator
         $borderX = 10.0;
         $borderY = 10.0;
 
-        $offsetTitleY = 40.0;
+        $offsetTicketIdY      = 50.0;
+        $offsetTicketTitleIdY = 65.0;
+
+        $fontSizeId    = 30.0;
+        $fontSizeTitle = 25.0;
+
+        $ticketTitleLineHeight = 30.0;
+
+
 
         // A6 dimensions are 297.64, 420.94
         $pdf = new FPDF('L', 'pt', 'A6');
 
         $pdf->AddPage();
 
-        $pdf->SetFont('Arial', 'B', 25.0);
+        $pdf->SetFont('Arial', 'B', $fontSizeId);
 
 
         $pdf->Rect(
@@ -37,14 +45,13 @@ class Service_PdfCreator
             $pageHeight - 2 * $borderY
         );
 
-
         $pdf->SetXY(0.0, 0.0);
-
-        //$pdf->Cell(500.0, 10.0, $ticketId, 'R');
-
         $titleWidth = $pdf->GetStringWidth($ticketId);
+        $pdf->Text(($pageWidth - $titleWidth) / 2, $offsetTicketIdY, $ticketId);
 
-        $pdf->Text(($pageWidth - $titleWidth) / 2, $offsetTitleY, $ticketId);
+        $pdf->SetXY($borderX, $offsetTicketTitleIdY);
+        $pdf->SetFont('Arial', '', $fontSizeTitle);
+        $pdf->MultiCell($pageWidth  - 2 * $borderX, $ticketTitleLineHeight, $ticketTitle, 0.0, 'C');
 
 
         //$pdf->Line(0.0, 0.0, $pageWidth, $pageHeight);
