955f16d99e3401a513a9ba185d9a750de667f836
Christopher Stock <email@christopherstock.de>
2016-10-21 19:06:15 +0200

    Packing DIN A 6 PDF.

--- src/Service/PdfCreator.php
+++ src/Service/PdfCreator.php
@@ -12,24 +12,42 @@ class Service_PdfCreator
     {
         $pdfOutName = 'out/pdfTest.pdf';
 
-        $width  = 420.94;
-        $height = 297.64;
+        // TODO refactor to constants!
+
+        $pageWidth  = 420.94;
+        $pageHeight = 297.64;
+
+        $borderX = 10.0;
+        $borderY = 10.0;
+
+        $offsetTitleY = 40.0;
 
         // A6 dimensions are 297.64, 420.94
         $pdf = new FPDF('L', 'pt', 'A6');
 
         $pdf->AddPage();
 
-        $pdf->SetFont('Arial', 'B', 20.0);
+        $pdf->SetFont('Arial', 'B', 25.0);
 
 
+        $pdf->Rect(
+            $borderX,
+            $borderY,
+            $pageWidth  - 2 * $borderX,
+            $pageHeight - 2 * $borderY
+        );
 
-        $pdf->Cell(0.0, 0.0, $ticketId, 'center');
 
-        $pdf->Line(0.0, 0.0, $width, $height);
+        $pdf->SetXY(0.0, 0.0);
+
+        //$pdf->Cell(500.0, 10.0, $ticketId, 'R');
+
+        $titleWidth = $pdf->GetStringWidth($ticketId);
+
+        $pdf->Text(($pageWidth - $titleWidth) / 2, $offsetTitleY, $ticketId);
 
 
-
+        //$pdf->Line(0.0, 0.0, $pageWidth, $pageHeight);
 
 
 
